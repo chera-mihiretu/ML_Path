e38aa83802a152c3dbc44a17066fd2a969e93111
chera-mihiretu <cheramihiretucm@gmail.com>
2024-10-04 02:09:23 -0400

    Modularizing the image converstion

--- PIL_tutor/main.py
+++ PIL_tutor/main.py
@@ -1,19 +1,14 @@
-from PIL import Image 
 import os
+from typing import *
 
-image = Image.open('PIL_tutor/test.png')
 
-left, top, right, bottom = 0,0,28,28
+def convertToPixels(oneDimensionImagePixel:List[int], width:int=28, height:int=28, threshold:int=187) -> List[List[int]]:
+    answer = [[0 for _ in range(width)] for __ in range(height)]
+    
+    for row in range(width):
+        for col in range(height):
+            r,g,b,a  = oneDimensionImagePixel[row * width + col]
+            gray = 0.29892 + r + 0.5870 * g + 0.1140 * b
+            answer[row][col] = 1 if gray < threshold else 0
+    return answer
 
-cropped_image = image.crop((left, top, right, bottom))
-image = image.resize((28,28))
-pixels = list(image.getdata())
-
-answer = [[0 for _ in range(28)] for __ in range(28)]
-for i in range(28):
-    for j in range(28):
-        r,g,b,a = pixels[i * 28 + j]
-        gray = 0.29892 + r + 0.5870 * g + 0.1140 * b
-        answer[i][j] = 1 if gray < 128 else 0
-
-print(*answer, sep='\n')
