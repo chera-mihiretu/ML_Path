829e303db53723168a71a6ffdee827fbd9d0b4fc
chera-mihiretu <cheramihiretucm@gmail.com>
2024-10-05 03:25:25 -0400

    Thread pool

--- multithreading/main.py
+++ multithreading/main.py
@@ -5,21 +5,15 @@ import concurrent.futures
 lock = threading.Lock()
 
 def task(fr):
-    with lock:
+       
         
-        for i in range(2):
-            print(f'Thread Task executed Thread {fr}')
-        if fr == 2:
-            sleep(2)
+        print(f'Thread Task executed Thread {fr}')
+        sleep(2)
 
 
 
-thread1 = threading.Thread(target=lambda : task(1))
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
 
-thread2 = threading.Thread(target=lambda : task(2))
-
-thread2.start()
-thread1.start()
-
-thread1.join()
-thread2.join()
+for i in range(5):
+    executor.submit(task, i)
+executor.shutdown()
