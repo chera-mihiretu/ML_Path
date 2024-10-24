96bce6656040f9ac998b75c277e060a3e4f9a8fb
chera-mihiretu <cheramihiretucm@gmail.com>
2024-10-24 09:28:18 -0400

    finishing watershading alforithm

--- watershade/watershadeAlgo.py
+++ watershade/watershadeAlgo.py
@@ -5,6 +5,8 @@ import numpy as np
 # This is image segmentation of an image 
 
 image = cv.imread('watershade/image3.png')
+
+print(image)
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 _, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV)
@@ -13,12 +15,14 @@ _, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV)
 contours, _ = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
 digits = []
+
+
 for contour in contours:
     x, y, w, h, = cv.boundingRect(contour)
 
     if w > 10 and h > 20:
         digit = binary[y:y+h, x:x+w]
-        digits.append(digit)
+        digits.append([x, digit])
 
         plt.imshow(digit)
         plt.show()
